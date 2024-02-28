a92e2c827c231d14c9b6afda20fdc1ba98de82ec
Kipras Vėberis <kipras.veberis1@gmail.com>
2024-02-28 18:54:17 +0200

    Main menu bug fix
    Fixed play button

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
