ed12bbc8246477c2d3cac0f66ff293922b16c2db
Kipras Vėberis <kipras.veberis1@gmail.com>
2024-03-01 21:56:57 +0200

    Added sound effects on menu buttons
    Now menu buttons have hover and click sounds.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.IO;
+using System.Threading;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource sound;
+    public AudioClip click;
+    public AudioClip hover;
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -14,5 +18,15 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ClickSound()
+    {
+        sound.PlayOneShot(click);
+    }
+
+        public void HoverSound()
+    {
+        sound.PlayOneShot(hover);
     }
 }
