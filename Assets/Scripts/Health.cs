32e857014c865d91bdc89291310c4f92ef456524
Steponas <steponas2003@gmail.com>
2024-03-05 13:40:55 +0200

    Added a working Healthbar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Heal(7);
+        }
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            // Die();
+        }
+    }
+    void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
