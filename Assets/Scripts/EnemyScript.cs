b8d522ead9ae2e152b2e5a4e7396036cd526f96c
rokaszak <rokaszakarauskas@gmail.com>
2024-03-10 11:01:20 +0200

    Enemy idle/run animation + sprite flip

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Animator anim;
+    private Vector3 moveDirectionAnim;
+
     [SerializeField]
     private int damage = 5;
     [SerializeField]
@@ -16,10 +19,13 @@ public class EnemyScript : MonoBehaviour
     private GameObject player;
     private float attackTimer; // Timer for tracking cooldown
 
+    bool facingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
         SetEnemyValues();
         attackTimer = 0f; // Initialize timer
     }
@@ -45,6 +51,28 @@ public class EnemyScript : MonoBehaviour
     private void Swarm()
     {
         transform.position = Vector2.MoveTowards(transform.position,player.transform.position,speed*Time.deltaTime);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float moveX = direction.x;
+        float moveY = direction.y;
+        if (moveX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        
+        if(moveX < 0 && facingRight)
+        {
+            Flip();
+        }
+        moveDirectionAnim = new Vector3(moveX,0, moveY);
+        if (moveDirectionAnim == Vector3.zero)
+        {
+            anim.SetFloat("Speed", 0);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0.1f);
+        }
     }
     private void OnTriggerStay2D(Collider2D collider)
     {
@@ -56,5 +84,14 @@ public class EnemyScript : MonoBehaviour
                 attackTimer = attackCooldown; // Reset the timer
             }
         }
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
