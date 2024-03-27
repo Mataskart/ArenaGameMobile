ce4a641456ebfc226844d93b23978d2b4ccae43c
Kipras Vėberis <kipras.veberis1@gmail.com>
2024-03-27 18:37:44 +0200

    added option to choose player

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+[System.Serializable]
+public class Fighter
+{
+    public string fighterName;
+    public Sprite fighterSprite;
+    public AnimatorController fighterAnimator;
+}
